2ff5414274870169cffa0bcbeb826e1d699ae0ec
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-08-01 16:54:26 +0300

    new file:   Frontend/public/images/admin.jpg 	new file:   Frontend/public/images/setting.png 	modified:   Frontend/src/components/DashboardScreen.jsx 	modified:   Frontend/src/components/Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -247,7 +247,7 @@ const Navbar = ({ onProfileClick }) => {
                   sx={{ display: 'flex', alignItems: 'center', color: 'white', textTransform: 'none' }}
                 >
                   <img
-                    src={userPhotoURL || "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"}
+                    src={userPhotoURL || "/public/images/admin.jpg"}
                     alt="User Profile"
                     style={{ height: '30px', width: '30px', borderRadius: '50%', marginRight: '10px' }}
                   />
